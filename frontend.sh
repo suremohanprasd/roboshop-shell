2f1bf30d861ec741325cdd85177f221917fdaec1
mohan <mohanprasad390@gmail.com>
2023-06-27 15:33:00 +0530

    frontend creation

---
+++ frontend.sh
@@ -0,0 +1,10 @@
+sudo su -
+set-hostname frontend
+yum install nginx -y
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
+systemctl enable nginx
