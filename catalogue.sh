ac5f773ac83fd26a3ace7092e364ce01bb15bd75
mohan <mohanprasad390@gmail.com>
2023-06-28 14:26:43 +0530

    crating all scripts

---
+++ catalogue.sh
@@ -0,0 +1,16 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+cp catalogue.sh /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
