0cd33c30f18b1d723c910364f3b1ec19bb56ae2a
mohan <mohanprasad390@gmail.com>
2023-07-07 00:04:10 +0530

    Frontend update

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,16 @@
+echo -e "\e[36m>>>>>>>>>> Install Nginx <<<<<<<<<<\e[0m"
 yum install nginx -y
+echo -e "\e[36m>>>>>>>>>> Copying Roboshop Configuration <<<<<<<<<<\e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[36m>>>>>>>>>> Removing content in web server <<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[36m>>>>>>>>>> Download the frontend content <<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[36m>>>>>>>>>> Extract the frontend <<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
+echo -e "\e[36m>>>>>>>>>> Unzipping the file <<<<<<<<<<\e[0m"
 unzip /tmp/frontend.zip
-systemctl restart nginx
+echo -e "\e[36m>>>>>>>>>> Enable Nginx <<<<<<<<<<\e[0m"
 systemctl enable nginx
+echo -e "\e[36m>>>>>>>>>> Restart Nginx <<<<<<<<<<\e[0m"
+systemctl restart nginx
