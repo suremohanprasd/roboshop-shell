c03c31f2f0e3bda2e733e85e03a7c730d0c111af
mohan <mohanprasad390@gmail.com>
2023-07-06 23:51:17 +0530

    IP Addresses Updated

--- redis.sh
+++ redis.sh
@@ -3,3 +3,6 @@ yum module enable redis:remi-6.2 -y
 yum install redis -y
 systemctl enable redis
 systemctl start redis
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
+systemctl restart redis
