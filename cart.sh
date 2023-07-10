5162b1d7ebc3a1f6322af8224d40e2ff3e485ea5
mohan <mohanprasad390@gmail.com>
2023-07-10 17:06:09 +0530

    cart

--- cart.sh
+++ cart.sh
@@ -3,6 +3,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo -e "\e[36m>>>>>>>>>> Install NodeJs <<<<<<<<<<\e[0m"
 yum install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 echo -e "\e[36m>>>>>>>>>> Download Application Code <<<<<<<<<<\e[0m"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
@@ -13,7 +14,7 @@ echo -e "\e[36m>>>>>>>>>> Download the Dependencies <<<<<<<<<<\e[0m"
 cd /app
 npm install
 echo -e "\e[36m>>>>>>>>>> Set SystemD Cart Service <<<<<<<<<<\e[0m"
-cp cart.service /etc/systemd/system/cart.service
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 systemctl daemon-reload
 systemctl enable cart
 systemctl start cart
